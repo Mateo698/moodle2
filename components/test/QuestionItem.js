e94551a790f7512dd28a5aceaf138f133a7c6288
Gianni <sb5021087@gmail.com>
2022-05-01 10:11:57 -0500

    change

--- components/test/QuestionItem.js
+++ components/test/QuestionItem.js
@@ -1,6 +1,7 @@
 export default function QuestionItem(props){
     return(
         <div>
+
             <h2>{props.number}</h2><br></br>
             <label>{props.statement}</label><br></br>
             
